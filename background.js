4f660c10a50816c3e49be8066f897c8058594d6d
Anthony Halliday <anthony.halliday@outlook.com>
2016-10-24 11:09:31 +0200

    Better overflow detection & visible support

--- background.js
+++ background.js
@@ -9,12 +9,41 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 function highlight(){
   var docWidth = document.documentElement.offsetWidth;
 
+  function isVisible(element){
+    var style = window.getComputedStyle(element);
+    
+    return style.display !== "none"
+        && style.visibility !== "hidden"
+        && style.opacity !== 0
+        && style.height !== 0
+        && style.width !== 0
+        
+        && (element.parentElement == null || isVisible(element.parentElement));
+  }
+
+  function getPosition(el) {
+    var x = 0;
+    var y = 0;
+    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+      x += el.offsetLeft - el.scrollLeft;
+      y += el.offsetTop - el.scrollTop;
+      el = el.offsetParent;
+    }
+    return { top: y, left: x };
+  }
+
   var results = [];
 
   document.querySelectorAll('*').forEach(function(el) {
-      if (el.offsetWidth > docWidth
-        || el.getBoundingClientRect().left + el.width > docWidth) {
+    
+    var elementPosition = getPosition(el);
+      if (elementPosition.left < 0 // Overflows left
+        || el.offsetWidth > docWidth // Element naturally overflows the width
+        || elementPosition.left + el.offsetWidth > docWidth) {
         
+        if(isVisible(el) === false)
+          return;
+
         // Highlight the element
         el.style.border = "1px dashed red";
 
@@ -25,8 +54,14 @@ function highlight(){
 
   if(results.length > 0){
 	
-	console.log('Elements overflowing the device width:')
-    console.table(results);
+	  console.log('Elements overflowing the device width:');
+    results.forEach(function(element){
+      console.log(element);
+    });
+    
+  }else{
+
+    console.log('No elements found overflowing the viewport :)')
   }
 
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # Dom Overflow Chrome plugin
 
-Chrome extension to highlight elements overflowing the device width.
+Chrome extension to highlight elements overflowing the device viewport.
 
 ## Credits
 - [project icon] Full Basket by Iulia Ardeleanu from the Noun Project
